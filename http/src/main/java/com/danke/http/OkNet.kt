98bfb4bfc0ad5f355ccfaad1b6793968dc5bfa8e
danke77 <xue_xiaozhe@163.com>
2018-06-25 15:19:33 +0800

    OkNet witch is http configs and manager

---
+++ http/src/main/java/com/danke/http/OkNet.kt
@@ -0,0 +1,124 @@
+package com.danke.http
+
+import com.danke.http.util.readWriteLazy
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.CallAdapter
+import retrofit2.Converter
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.ArrayList
+import java.util.concurrent.TimeUnit
+
+/**
+ * @author danke
+ * @date 2018/6/19
+ */
+object OkNet {
+
+    @JvmStatic
+    var baseUrl: String = ""
+
+    @JvmStatic
+    var timeout: Long = 15
+
+    @JvmStatic
+    var clientBuilder: OkHttpClient.Builder? = null
+
+    @JvmStatic
+    var isLoggingEnable: Boolean = false
+
+    private val converterFactories: ArrayList<Converter.Factory> = arrayListOf(GsonConverterFactory.create())
+
+    private val callAdapterFactories: ArrayList<CallAdapter.Factory> = arrayListOf(RxJava2CallAdapterFactory.create())
+
+    private val interceptors: ArrayList<Interceptor> = arrayListOf()
+
+    private val retrofit: Retrofit by readWriteLazy { createRetrofit() }
+
+    private fun createHttpClient(): OkHttpClient {
+        val builder = clientBuilder ?: OkHttpClient.Builder()
+        var isHttpLoggingInterceptorAdded = false
+
+        interceptors.forEach {
+            if (it is HttpLoggingInterceptor) {
+                isHttpLoggingInterceptorAdded = true
+            }
+            builder.addInterceptor(it)
+        }
+
+        if (!isHttpLoggingInterceptorAdded) {
+            builder.addInterceptor(HttpLoggingInterceptor().setLevel(
+                    if (isLoggingEnable) HttpLoggingInterceptor.Level.BODY else HttpLoggingInterceptor.Level.NONE))
+        }
+
+        builder.connectTimeout(timeout, TimeUnit.SECONDS)
+                .readTimeout(timeout, TimeUnit.SECONDS)
+                .writeTimeout(timeout, TimeUnit.SECONDS)
+
+        return builder.build()
+    }
+
+    private fun createRetrofit(): Retrofit {
+        val builder = Retrofit.Builder()
+                .client(createHttpClient())
+                .baseUrl(baseUrl)
+
+        converterFactories.forEach { builder.addConverterFactory(it) }
+        callAdapterFactories.forEach { builder.addCallAdapterFactory(it) }
+
+        return builder.build()
+    }
+
+    @JvmStatic
+    fun addConverterFactory(vararg factory: Converter.Factory) {
+        converterFactories += factory
+    }
+
+    @JvmStatic
+    fun removeConverterFactory(vararg factory: Converter.Factory) {
+        converterFactories -= factory
+    }
+
+    @JvmStatic
+    fun removeAllConverterFactories() {
+        converterFactories.clear()
+    }
+
+    @JvmStatic
+    fun addCallAdapterFactory(vararg factory: CallAdapter.Factory) {
+        callAdapterFactories += factory
+    }
+
+    @JvmStatic
+    fun removeCallAdapterFactory(vararg factory: CallAdapter.Factory) {
+        callAdapterFactories -= factory
+    }
+
+    @JvmStatic
+    fun removeAllCallAdapterFactories() {
+        callAdapterFactories.clear()
+    }
+
+    @JvmStatic
+    fun addInterceptor(vararg interceptor: Interceptor) {
+        interceptors += interceptor
+    }
+
+    @JvmStatic
+    fun removeInterceptor(vararg interceptor: Interceptor) {
+        interceptors -= interceptor
+    }
+
+    @JvmStatic
+    fun removeAllInterceptors() {
+        interceptors.clear()
+    }
+
+    @JvmStatic
+    fun <T> create(serviceClazz: Class<T>): T {
+        return retrofit.create(serviceClazz)
+    }
+}
---
+++ http/src/main/java/com/danke/http/util/Delegates.kt
@@ -0,0 +1,24 @@
+package com.danke.http.util
+
+import kotlin.properties.ReadWriteProperty
+import kotlin.reflect.KProperty
+
+internal fun <T> readWriteLazy(initializer: () -> T): ReadWriteProperty<Any?, T> = ReadWriteLazyVal(initializer)
+
+private class ReadWriteLazyVal<T>(private val initializer: () -> T) : ReadWriteProperty<Any?, T> {
+
+    private var value: Any? = null
+
+    override operator fun getValue(thisRef: Any?, property: KProperty<*>): T {
+        if (value == null) {
+            value = (initializer()) ?: throw IllegalStateException("Initializer block of property ${property.name} return null")
+        }
+        @Suppress("UNCHECKED_CAST")
+        return value as T
+    }
+
+    override operator fun setValue(thisRef: Any?, property: KProperty<*>, value: T) {
+        this.value = value
+    }
+
+}
