3474b7cfe42d3642664255695cc8d9da33dfa455
danke77 <xue_xiaozhe@163.com>
2018-06-27 09:59:52 +0800

    get and post sample

---
+++ app/src/main/java/com/danke/http/sample/ApiService.kt
@@ -0,0 +1,24 @@
+package com.danke.http.sample
+
+import io.reactivex.Observable
+import retrofit2.Response
+import retrofit2.http.*
+
+/**
+ * @author danke
+ * @date 2018/6/27
+ */
+interface ApiService {
+
+    @GET("top250")
+    fun getTop250(@Query("start") start: Int = 0,
+                  @Query("count") count: Int = 10): Observable<Response<MovieResponse>>
+
+    @FormUrlEncoded
+    @POST("top250")
+    fun postTop250(@Field("start") start: Int = 0,
+                   @Field("count") count: Int = 10): Observable<Response<MovieResponse>>
+
+    @GET("subject/{id}")
+    fun get(@Path("id") id: Long): Observable<Response<MovieResponse>>
+}
